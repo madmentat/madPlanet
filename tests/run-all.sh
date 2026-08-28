ab9f8a89acd9fa1002e91b2d19be87eac98fc808
madmentat <mypost@madmentat.ru>
2026-08-29 01:51:37 +0300

    0.5.47: добавить soil hydrology в общий regression-run

--- tests/run-all.sh
+++ tests/run-all.sh
@@ -31,6 +31,7 @@ node tests/h2o-advection.test.js
 node tests/condensation.test.js
 node tests/precipitation.test.js
 node tests/orographic-lift.test.js
+node tests/soil-hydrology.test.js
 node tests/touch-ux.test.js
 node tests/chromium-compat.test.js
 node tests/hydrology.test.js
