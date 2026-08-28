9eceb859a978ac2ff5ed623c12e613f8a2576e0e
madmentat <mypost@madmentat.ru>
2026-08-29 01:29:19 +0300

    0.5.45: добавить precipitation test в run-all

--- tests/run-all.sh
+++ tests/run-all.sh
@@ -29,6 +29,7 @@ node tests/baric-field.test.js
 node tests/wind-dynamics.test.js
 node tests/h2o-advection.test.js
 node tests/condensation.test.js
+node tests/precipitation.test.js
 node tests/touch-ux.test.js
 node tests/chromium-compat.test.js
 node tests/hydrology.test.js
