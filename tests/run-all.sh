bec7b6be6acd8c3e32f3d0556cd5422bc80bac10
madmentat <mypost@madmentat.ru>
2026-08-29 01:18:28 +0300

    0.5.44: condensation test в общий прогон

--- tests/run-all.sh
+++ tests/run-all.sh
@@ -28,6 +28,7 @@ node tests/local-energy-balance.test.js
 node tests/baric-field.test.js
 node tests/wind-dynamics.test.js
 node tests/h2o-advection.test.js
+node tests/condensation.test.js
 node tests/touch-ux.test.js
 node tests/chromium-compat.test.js
 node tests/hydrology.test.js
