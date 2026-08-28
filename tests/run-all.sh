04b60258065a96259ad22dd58932ccdc224fcb6f
madmentat <mypost@madmentat.ru>
2026-08-28 23:21:18 +0300

    0.5.37: тест воды добавлен в общий прогон

--- tests/run-all.sh
+++ tests/run-all.sh
@@ -19,6 +19,7 @@ node tests/param-model.test.js
 node tests/star-orbit.test.js
 node tests/planet-physics.test.js
 node tests/atmosphere-inventory.test.js
+node tests/water-budget.test.js
 node tests/touch-ux.test.js
 node tests/chromium-compat.test.js
 node tests/hydrology.test.js
