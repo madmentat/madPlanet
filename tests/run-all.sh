be7be9a1a363cb1583524fcacddaa6880727c9db
madmentat <mypost@madmentat.ru>
2026-08-29 01:08:45 +0300

    0.5.43: включаем H2O test в общий прогон

--- tests/run-all.sh
+++ tests/run-all.sh
@@ -27,6 +27,7 @@ node tests/weather-core.test.js
 node tests/local-energy-balance.test.js
 node tests/baric-field.test.js
 node tests/wind-dynamics.test.js
+node tests/h2o-advection.test.js
 node tests/touch-ux.test.js
 node tests/chromium-compat.test.js
 node tests/hydrology.test.js
