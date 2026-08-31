cacbcf46b87ca56a529a44aa7c17968c2df115de
madmentat <mypost@madmentat.ru>
2026-08-31 19:17:52 +0300

    test: run smooth motion regression

--- tests/run-all.sh
+++ tests/run-all.sh
@@ -24,6 +24,7 @@ rm -f .built-script.js
 tests=(
   tests/build-integrity.test.js
   tests/performance-guards.test.js
+  tests/smooth-motion-ui.test.js
   tests/visual-regressions.test.js
   tests/tectonic-morphology.test.js
   tests/tectonic-cell-seams.test.js
