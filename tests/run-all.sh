0eddce66fcf565d59ed06c030dc01c3c66f7b78b
madmentat <mypost@madmentat.ru>
2026-09-02 14:31:43 +0300

    Run polar continentality regression

--- tests/run-all.sh
+++ tests/run-all.sh
@@ -69,6 +69,7 @@ tests=(
   tests/ocean-heat-transport.test.js
   tests/atmospheric-heat-transport.test.js
   tests/polar-surface-thermodynamics.test.js
+  tests/polar-continentality.test.js
   tests/cryosphere-seams.test.js
   tests/cryosphere-hard-edge.test.js
   tests/physical-fog.test.js
